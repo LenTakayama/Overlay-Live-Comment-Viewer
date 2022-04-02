021e98a15fb5e33daddaf7d4e60bc7e4a4f53795
LenTakayama <teikailen@gmail.com>
2022-04-02 21:14:21 +0900

    判定のバグを修正

--- src/window/utility.ts
+++ src/window/utility.ts
@@ -1,8 +1,10 @@
 import { BrowserWindow } from 'electron';
 
-export function closeWindow(window: BrowserWindow): void {
-  window.close;
-  if (window.isDestroyed()) {
-    window.destroy();
+export function closeWindow(window?: BrowserWindow): void {
+  if (window) {
+    window.close();
+    if (!window.isDestroyed()) {
+      window.destroy();
+    }
   }
 }
