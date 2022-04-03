2adc75a6e93559263b06a8c2c2106f8dbe473817
LenTakayama <teikailen@gmail.com>
2022-04-03 13:09:19 +0900

    コード改善

--- src/component/log.ts
+++ src/component/log.ts
@@ -6,8 +6,9 @@ log.transports.console.level =
   process.env.NODE_ENV == 'development' ? 'debug' : 'info';
 log.transports.file.level = 'info';
 log.transports.file.resolvePath = (variables: ElectronLog.PathVariables) => {
-  if (variables.electronDefaultDir && variables.fileName) {
-    return join(variables.electronDefaultDir, variables.fileName);
+  if (variables.electronDefaultDir) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return join(variables.electronDefaultDir, variables.fileName!);
   } else {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return join(variables.libraryDefaultDir, variables.fileName!);
